b67687867608e2278c37232a3fdd91c9dc86b3bb
Artem-ne-Artem <69469785+Artem-ne-Artem@users.noreply.github.com>
2022-11-04 15:05:39 +0600

    Update datamart_query.sql

--- datamart_query.sql
+++ datamart_query.sql
@@ -12,15 +12,15 @@ left join	analysis.tmp_rfm_frequency as f on u.user_id = f.user_id
 left join	analysis.tmp_rfm_monetary_value as m on u.user_id = m.user_id
 left join	analysis.tmp_rfm_recency as r on u.user_id = r.user_id
 ---------------------------------------------------------------------------------------
-| user_id | recency | frequency | monetary_value
-| 0       | 1       | 3         | 4
-| 1       | 4       | 3         | 3
-| 2       | 2       | 3         | 5
-| 3       | 2       | 3         | 3
-| 4       | 4       | 3         | 3
-| 5       | 5       | 5         | 5
-| 6       | 1       | 3         | 5
-| 7       | 4       | 2         | 2
-| 8       | 1       | 1         | 3
-| 9       | 1       | 2         | 2
-| 10      | 3       | 4         | 2
+| user_id | recency | frequency | monetary_value |
+| 0 | 1 | 3 | 4 |
+| 1 | 4 | 3 | 3 |
+| 2 | 2 | 3 | 5 |
+| 3 | 2 | 3 | 3 |
+| 4 | 4 | 3 | 3 |
+| 5 | 5 | 5 | 5 |
+| 6 | 1 | 3 | 5 |
+| 7 | 4 | 2 | 2 |
+| 8 | 1 | 1 | 3 |
+| 9 | 1 | 2 | 2 |
+| 10 | 3 | 4 | 2 |
