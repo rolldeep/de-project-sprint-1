8705bca14786c6bbd13ccd2871828b90c012f48f
Artem-ne-Artem <69469785+Artem-ne-Artem@users.noreply.github.com>
2022-11-04 14:53:01 +0600

    Create orders_view.sql

---
+++ orders_view.sql
@@ -0,0 +1,9 @@
+create view de.analysis.orders as
+select		order_id ,status_id
+from		(select		order_id ,status_id ,dttm
+						,max(dttm) over(partition by order_id) as max_dt
+			from		de.production.OrderStatusLog
+			order by	order_id
+			) as tbl
+where		dttm = max_dt
+;
